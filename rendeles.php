5a848fcbdd72c50508d277ec64273aaf4a3da975
DLoomz <37878610+DLoomz@users.noreply.github.com>
2021-04-17 16:07:41 +0200

    a

--- rendeles.php
+++ rendeles.php
@@ -23,5 +23,21 @@
       <li><a href="signup.php" class = "menu" target = "_blank">Regisztráció</a></li>
     <?php } ?>
     </ul><br/></div>
+    <form action="rendeles.php" method="POST">
+    <fieldset class="urlap">
+      <legend>Rendelési adatok</legend>
+      <label>Teljes név: <input type="text" name="nev"/></label> <br/>
+      <label>Város: <input type="text" name="varos"/></label> <br/>
+      <label>Utca: <input type="text" name="utca"/></label> <br/>
+      <label>Hazszam: <input type="number" name="szam"/></label> <br/>
+      Milyen burgert szeretne vásárolni?
+      <label><input type="checkbox" name="burger[]" value="sajtburesz"/> Sajtburesz</label>
+      <label><input type="checkbox" name="burger[]" value="szpajsziburger"/> Szpájsziburger</label>
+      <label><input type="checkbox" name="burger[]" value="mekrojal"/> Mekrojál</label>
+      <label><input type="checkbox" name="burger[]" value="vegaburger"/> Vegaburger</label>
+      <label><input type="checkbox" name="burger[]" value="pitburger"/> Pitburger</label> <br/>
+    </fieldset>
+    <input type="submit" name="rendel" value="Rendelés"/> <br/>
+ </form>
   </body>          
 </html>
